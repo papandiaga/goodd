9cb184e496e1757aa9b4a3089a030324176f5acb
papandiaga <71021254+papandiaga@users.noreply.github.com>
2021-12-15 12:24:46 -0500

    nothingyet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Hello React App</h1>
+        <h1>Yes</h1>
       </div>
     );
   }
